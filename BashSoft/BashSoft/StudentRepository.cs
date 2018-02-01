c9b526bcdca1d1b4d173bb3523b3eaa340e22b0d
inser788 <inser7@abv.bg>
2018-02-01 22:42:13 +0200

    add initial commit

---
+++ BashSoft/BashSoft/StudentRepository.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BashSoft
+{
+    public static class StudentRepository
+    {
+        public static bool isDataInitialized = false;
+        private static Dictionary<string, Dictionary<string, List<int>>> studentsByCourse;
+
+        public static void InitializeData()
+        {
+            if (!isDataInitialized)
+            {
+                OutputWriter.WriteMessageOnNewLine("Reading data...");
+                studentsByCourse=new Dictionary<string, Dictionary<string, List<int>>>();
+                ReadData();
+            }
+            else
+            {
+                OutputWriter.WriteMessageOnNewLine(ExeptionMessages.DataAlreadyInitializedException);
+            }
+        }
+
+        private static void ReadData()
+        {
+            string input = Console.ReadLine();
+
+            while (!string.IsNullOrEmpty(input))
+            {
+                string[] tokens = input.Split();
+                string course = tokens[0];
+                string student = tokens[1];
+                int mark = int.Parse(tokens[2]);
+
+                if (!studentsByCourse.ContainsKey(course))
+                {
+                    studentsByCourse.Add(course,new Dictionary<string, List<int>>());
+                }
+
+                if (!studentsByCourse[course].ContainsKey(student))
+                {
+                    studentsByCourse[course].Add(student,new List<int>());
+                }
+                studentsByCourse[course][student].Add(mark);
+                input = Console.ReadLine();
+            }
+
+            isDataInitialized = true;
+            OutputWriter.WriteMessageOnNewLine("Data read!");
+        }
+
+        private static bool IsQueryForCoursePossible(string courseName)
+        {
+            if (isDataInitialized)
+            {
+                if (studentsByCourse.ContainsKey(courseName))
+                {
+                return true;
+                }
+                else
+                {
+                    OutputWriter.DisplayException(ExeptionMessages.InexistingCourseInDataBase);
+                }
+            }
+            else
+            {
+                OutputWriter.DisplayException(ExeptionMessages.DataNotInitializedExceptionMessage);
+            }
+
+            return false;
+        }
+    }
+}
