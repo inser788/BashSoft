8d3f414fe8b08752773ea984667ab0f83e010fe0
inser788 <inser7@abv.bg>
2018-03-06 16:15:58 +0200

    make test migration from .Framework to .Core

--- BashSoft/BashSoft/Startup.cs
+++ BashSoft/BashSoft/Startup.cs
@@ -1,32 +1,11 @@
-﻿namespace BashSoft
+﻿using System;
+
+namespace BashSoft
 {
-    public class Startup
+    class StartUp
     {
-        public static void Main()
+        static void Main()
         {
-            // Interim steps
-
-            //IOManager.TraverseDirectory(@"C:\Users\A\Documents\GitHub\C-Advanced\BashSoft");
-
-            //StudentsRepository.InitializeData();
-            //StudentsRepository.GetAllStudentsFromCourse("Unity");
-            //StudentsRepository.GetStudentScoresFromCourse("Unity", "Ivan");
-
-            //Tester.CompareContent(@"C:\Users\A\Documents\GitHub\C-Advanced\BashSoft\data\test2.txt", @"C:\Users\A\Documents\GitHub\C-Advanced\BashSoft\data\test3.txt");
-
-            //IOManager.CreateDirectoryInCurrentFolder("../../../pesho");
-            //IOManager.TraverseDirectory();
-            //IOManager.TraverseDirectory(2);
-
-            //IOManager.ChangeCurrentDirectoryAbsolute(@"C:\Windows");
-            //IOManager.TraverseDirectory(1);
-
-            //IOManager.CreateDirectoryInCurrentFolder("*2");
-
-            //IOManager.ChangeCurrentDirectoryRelative("..");
-            //IOManager.ChangeCurrentDirectoryRelative("..");
-            //IOManager.ChangeCurrentDirectoryRelative("..");
-
             InputReader.StartReadingCommands();
         }
     }
